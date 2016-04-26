0de6f9f964488b9389f63a0cd49df3e489276bdd
TC94615 <tclee0117@gmail.com>
2016-04-26 16:52:14 +0800

    Add category UIColor+Util.

---
+++ Pod/Classes/UIColor+Util.h
@@ -0,0 +1,14 @@
+//
+// Created by liq on 12/19/13.
+// Copyright (c) 2013 Liq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UIColor(Util)
++ (UIColor *) hexRGB:(int) rgbHex;
+
++ (UIColor *) hexARGB:(int) argbHex;
+
+@end
